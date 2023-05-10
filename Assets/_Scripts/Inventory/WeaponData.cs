50bd076f658aa28cefe6ab2aed9c8a0def6db85d
Supremetor <brettzky007@gmail.com>
2023-05-10 17:24:55 -0500

    Added Items
    - Made Item and Weapon data objects
    - Imported sprite assets

---
+++ Assets/_Scripts/Inventory/WeaponData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to define the category this weapon is in
+/// </summary>
+[System.Serializable]
+public enum WeaponType { Sword, Lance, Axe }
+
+/// <summary>
+/// Used to define which stats should be used for damage calculation
+/// </summary>
+[System.Serializable]
+public enum DamageType { Physical, Energy, Highest }
+
+/// <summary>
+/// Used to define information about a weapon
+/// </summary>
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Items/Weapon")]
+public class WeaponData : ItemData
+{
+    public WeaponType WeaponType; // The category this weapon is a part of
+    public DamageType DamageType; // The category of damage this weapon will inflict
+
+    public int Might;   // Used for damage calculation
+    public int Weight;  // Used for speed calculation
+}
